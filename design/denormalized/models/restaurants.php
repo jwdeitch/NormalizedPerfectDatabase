477720a153e9a2129b5dbec6141b609d9455b2d9
jwdeitch <jwdeitch@gmail.com>
2016-03-22 22:17:19 -0400

    denormalized example - mwb files

---
+++ design/denormalized/models/restaurants.php
@@ -0,0 +1,11 @@
+<?php
+namespace Denormalized;
+
+use Illuminate\Database\Eloquent\Model as model;
+
+class restaurants extends model {
+
+    public $guarded = [];
+    public $timestamps = false;
+
+}
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 require 'vendor/autoload.php';
 require 'config.php';
-require 'bootstrap.php';
+require 'database.php';
